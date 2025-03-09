396a9f1958e74037dd3f9567fedeea7064f9ce49
Pranaygollar8738 <pranaygollar8738@gmail.com>
2025-03-09 08:58:49 +0530

    09March Example02 Added

--- src/practice01/Example02.java
+++ src/practice01/Example02.java
@@ -1,7 +1,7 @@
 package practice01;
 
-public class Example_01 {
+public class Example02 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("hiiii");
     }
 }
