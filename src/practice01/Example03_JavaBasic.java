48da1560e6d60a487dc045192772920309795b99
Pranaygollar8738 <pranaygollar8738@gmail.com>
2025-03-10 19:05:48 +0530

    10th March

---
+++ src/practice01/Example03_JavaBasic.java
@@ -0,0 +1,10 @@
+package practice01;
+
+public class Example03_JavaBasic {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2-3);
+        System.out.println(3*3);
+        System.out.println(5/6);
+    }
+}
