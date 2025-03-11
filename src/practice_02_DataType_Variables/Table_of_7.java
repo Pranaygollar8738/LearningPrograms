101ec840f2db8e5cada58566f45c8f6f66613061
Pranaygollar8738 <pranaygollar8738@gmail.com>
2025-03-11 12:30:17 +0530

    11March Data type and Variable, Table of 7 without for loop

---
+++ src/practice_02_DataType_Variables/Table_of_7.java
@@ -0,0 +1,31 @@
+package practice_02_DataType_Variables;
+
+public class Table_of_7 {
+    public static void main(String[] args) {
+        int a = 1;
+        System.out.printf("%d*7=%d\n", a, a*7);
+        a++;
+        System.out.printf("%d*7=%d\n", a, a*7);
+        a++;
+        System.out.printf("%d*7=%d\n", a, a*7);
+        a++;
+        System.out.printf("%d*7=%d\n", a, a*7);
+        a++;
+        System.out.printf("%d*7=%d\n", a, a*7);
+        a++;
+        System.out.printf("%d*7=%d\n", a, a*7);
+        a++;
+        System.out.printf("%d*7=%d\n", a, a*7);
+        a++;
+        System.out.printf("%d*7=%d\n", a, a*7);
+        a++;
+        System.out.printf("%d*7=%d\n", a, a*7);
+        a++;
+        System.out.printf("%d*7=%d\n", a, a*7);
+        a++;
+
+
+
+    }
+
+}
